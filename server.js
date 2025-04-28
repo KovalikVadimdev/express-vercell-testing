ac2c8085a2a76df246b306ee048a8684181c7c99
KovalikVadimdev <124208880+KovalikVadimdev@users.noreply.github.com>
2025-04-29 01:14:22 +0300

    feat: Adding new route profile

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const menuRoutes = require("./routes/menu");
 const ordersRoutes = require("./routes/orders");
 const reviewsRoutes = require("./routes/reviews");
 const advantagesRoutes = require("./routes/advantages");
+const authMiddleware = require("./middleware/authMiddleware");
 require("dotenv").config();
 
 const app = express();
@@ -19,6 +20,10 @@ app.use("/api/menu", menuRoutes);
 app.use("/api/orders", ordersRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/advantages", advantagesRoutes);
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+  res.json({ message: `Hello, user with email ${req.user.email}` });
+});
 
 app.listen(3000, () => {
   console.log(`Server started on port ${3000}`);
